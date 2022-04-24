92f978ba4fb2586a30d544fe774c2c608dea9a4f
Firuz43 <firuz1199@mail.ru>
2022-04-24 15:10:47 +0400

    src

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int age = 30;
+        age = 35;
+        System.out.println(age);
+    }
+}
